81e5cd0c0d3f90f64b6ddaa09f8a7440c8650c4f
liza051 <lizaletvinenko331@gmail.com>
2025-12-27 22:04:33 +0200

    Update 2-for-of.js

--- lab-4/2-for-of.js
+++ lab-4/2-for-of.js
@@ -10,7 +10,7 @@ const e = sum(10, -1, -1, -1); // e === 7
 console.log(a, b, c, d, e);
 
 // виконання
-function sum(...args) {
+function sum(...args)  {
   let sum = 0;
   for (let arg of args) {
     sum += arg;
